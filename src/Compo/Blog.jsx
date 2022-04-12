a1905ca378a9416870c4160d7f5e06bcab8ed15f
maanp <87629198+maanp@users.noreply.github.com>
2022-04-12 22:06:18 +0530

    Update Blog.jsx

--- src/Compo/Blog.jsx
+++ src/Compo/Blog.jsx
@@ -114,7 +114,7 @@ export const Blog = ({ blog, theme }) => {
                         {blog.title}
                     </Typography>
                     <Typography variant='body2' color="text.secondary">
-                        {blog.desc.slice(0,440)}
+                        {blog.desc.slice(0,440)}...
                     </Typography>
                 </CardContent>
                 <CardActions>
