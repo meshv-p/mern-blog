43d942590b8ed86a32e68590cfc3a99c9ebf9d96
Meshv Patel <meshv1823@gmail.com>
2022-05-20 09:27:31 +0530

    Change url

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -7,7 +7,7 @@ export const apiSlice = createApi({
   reducerPath: "api",
   // All of our requests will have URLs starting with '/fakeApi'
   baseQuery: fetchBaseQuery({
-    baseUrl: url,
+    baseUrl: `${url}/api/v1/`,
   }),
   // The "endpoints" represent operations and requests for this server
   endpoints: (builder) => ({
